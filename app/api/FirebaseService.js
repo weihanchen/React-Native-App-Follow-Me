2de51375ebfc304a8a4128d6f88b57e5db40efa6
weihanchen <willhanchen@gmail.com>
2016-12-28 22:59:45 +0800

    add firebase service

---
+++ app/api/FirebaseService.js
@@ -0,0 +1,10 @@
+import * as Firebase from 'firebase'
+const firebaseConfig = {
+    databaseUrl: 'https://willapp.firebaseio.com'
+}
+Firebase.initializeApp(firebaseConfig)
+export default class FirebaseService {
+  constructor() {
+    this.rootRef = Firebase.ref('followme')
+  }
+}
